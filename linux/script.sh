8ef3b289157c275c4116cd60df46b3ad0f4e538c
Pablo Sousa <pablosousa.ads@gmail.com>
2020-11-24 17:23:59 -0300

    adicionado nodejs 14 e yarn ao script

--- linux/script.sh
+++ linux/script.sh
@@ -25,10 +25,18 @@ echo "Installing python..."
 sudo apt-get install python3
 sudo apt-get install python3-pip
 
-echo "Installing node..."
+echo "Installing node,npm,yarn..."
 # node
-sudo apt install nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+cat /etc/apt/sources.list.d/nodesource.list
+sudo apt -y install nodejs
+sudo apt -y install gcc g++ make
+# npm
 sudo apt install npm
+# yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
 
 echo "Installing php..."
 # php
