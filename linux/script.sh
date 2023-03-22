960cd635cd82720a5c6642545ca702dcbff2c8d9
Pablo Sousa <pablosousa.ads@gmail.com>
2023-03-22 07:31:16 -0300

    up

--- linux/script.sh
+++ linux/script.sh
@@ -64,3 +64,4 @@ echo "Installing docker..."
 sudo apt install docker.io
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chown $USER /var/run/docker.sock
